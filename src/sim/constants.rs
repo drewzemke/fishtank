9a2677635cb13ac397d4d910d48cfb8e606ef33e
Drew Deleón-Zemke <drew.zemke@gmail.com>
2025-12-19 15:16:08 -0800

    chore: tweak constants -- getting there!

--- src/sim/constants.rs
+++ src/sim/constants.rs
@@ -1,6 +1,6 @@
-pub const TIMESTEP_MS: u64 = 20;
+pub const TIMESTEP_MS: u64 = 10;
 
-pub const GRAVITY: f64 = 20.;
+pub const GRAVITY: f64 = 2.;
 
 // collision model
 pub const DAMPENING: f64 = 0.01;
@@ -14,9 +14,10 @@ pub const PARTICLE_MASS: f64 = 1.;
 
 pub const TARGET_DENSITY: f64 = 0.5;
 
-pub const STIFFNESS: f64 = 200.;
+// FIXME: this is used more like an inverse stiffness
+pub const STIFFNESS: f64 = 300.;
 
-pub const VISCOSITY: f64 = 0.5;
+pub const VISCOSITY: f64 = 1.;
 
 // spatial hashing
 pub const CELL_SIZE: f64 = SMOOTHING_RADIUS;
